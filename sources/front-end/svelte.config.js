5e9cf13e8cd2dc78449537d8e4feda010ceb6f1d
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-03 19:18:02 +0100

    changed svelte.config

--- sources/front-end/svelte.config.js
+++ sources/front-end/svelte.config.js
@@ -1,8 +1,16 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		format: 'esm',
+		hydratable: true,
+	},
+	extensions: [ '.svelte' ],
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		// hydrate the <div id="macaroons-berlin"> element in src/app.html
+		target: '#macaroons-berlin',
+		hydrate: true,
+		router: true,
+		ssr: true,
 	}
 };
 
