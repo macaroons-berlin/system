da4109fa7a50f3c3a6eb572d6c28e4ff93e6b143
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-04 20:43:19 +0100

    added font and changed styles a bit

--- sources/front-end/src/lib/header/Header.svelte
+++ sources/front-end/src/lib/header/Header.svelte
@@ -1,6 +1,5 @@
 <script>
   import { page } from '$app/stores';
-  // import logo from './svelte-logo.svg';
 </script>
 
 <style>
@@ -18,7 +17,6 @@
   nav {
     display: flex;
     justify-content: center;
-    /* --background: rgba(255, 255, 255, 0.7); */
   }
 
   ul {
@@ -37,20 +35,12 @@
   ul > li {
     position: relative;
     height: 6rem;
-    min-width: 9rem;
   }
 
-  /* ul > li.active::before {
-    --size: 6px;
-    content: '';
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--accent-color);
-  } */
+  ul > li:not(:first-of-type) {
+    margin-left: 1rem;
+  }
+
 
   ul > li > a {
     display: flex;
@@ -59,16 +49,6 @@
     height: 100%;
     width: 100%;
     padding: 0;
-    color: var(--heading-color);
-    font-weight: 700;
-    font-size: 0.8rem;
-    text-transform: uppercase;
-    letter-spacing: 0.1em;
-    text-decoration: none;
-    transition: color 0.2s linear;
-
-    font-size: 1.5rem;
-    pointer-events: all;
   }
 
 
@@ -82,9 +62,9 @@
     <ul>
       <!-- <li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li> -->
       <!-- <li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">about</a></li> -->
-      <li class:active={$page.path === '/search'}><a sveltekit:prefetch href="/search">search</a></li>
-      <li class:active={$page.path === '/profile'}><a sveltekit:prefetch href="/profile">profile</a></li>
-      <li class:active={$page.path === '/basket'}><a sveltekit:prefetch href="/basket">basket</a></li>
+      <li class:active={$page.path === '/search'}><a sveltekit:prefetch href="/search" class="nav-text">search</a></li>
+      <li class:active={$page.path === '/profile'}><a sveltekit:prefetch href="/profile" class="nav-text">profile</a></li>
+      <li class:active={$page.path === '/basket'}><a sveltekit:prefetch href="/basket" class="nav-text">basket</a></li>
     </ul>
   </nav>
 </header>
