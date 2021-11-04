1efe2b48f6dae4626a925298771eccf942faf22b
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-04 10:29:55 +0100

    changed styles a bit + layout

---
+++ sources/front-end/src/lib/footer/Footer.svelte
@@ -0,0 +1,17 @@
+<style>
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+  }
+
+  footer a {
+    font-weight: bold;
+  }
+</style>
+
+<footer>
+  <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
