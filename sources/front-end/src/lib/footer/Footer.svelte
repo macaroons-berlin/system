33b66a986007724a650d4153817b7298008979a4
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-05 19:49:12 +0100

    more footer + internal messaging for newsletter subscription

--- sources/front-end/src/lib/footer/Footer.svelte
+++ sources/front-end/src/lib/footer/Footer.svelte
@@ -1,16 +1,40 @@
 <script>
   import Newsletter from '$lib/controls/newsletter/Newsletter.svelte';
+  import List from '$lib/controls/list/List.svelte';
 </script>
 
 <style>
   footer {
-  display: grid;
-  grid-template-columns: 0.75fr repeat(4, 1fr) 0.75fr;
-  grid-template-rows: 4rem 1fr 4rem;
-  grid-template-areas:
-    '. . newsletter newsletter . .'
-    '. help company follow-us other .'
-    '. info info info info .';
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: 0.125fr auto 1fr auto 0.125fr;
+    grid-template-areas:
+      '. . . . .'
+      '. newsletter newsletter newsletter .'
+      '. help company follow-us .'
+      '. legal legal legal .'
+      '. . . . .';
+    row-gap: 2rem;
+    column-gap: 1.5rem;
+  }
+
+  .newsletter,
+  .help,
+  .company,
+  .follow-us {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .newsletter,
+  .help,
+  .company,
+  .follow-us,
+  .legal a {
+    text-decoration: none;
+    text-transform: capitalize;
+    letter-spacing: 0.1em;
+    color: var(--text-color);
   }
 
   .newsletter {
@@ -29,45 +53,60 @@
     grid-area: follow-us;
   }
 
-  .other {
-    grid-area: other;
-  }
-
-  .info {
-    grid-area: info;
+  .legal {
+    grid-area: legal;
   }
 </style>
 
 <footer>
   <div class='newsletter'><Newsletter /></div>
   <div class='help'>
-    <ul>
-      <li>frequently asked questions</li>
-      <li>how to purchase</li>
-      <li>transport and delivery</li>
-      <li>payments</li>
-      <li>contact</li>
-    </ul>
+    <List id='help-list' caption='help'>
+      <li>
+        <a href='/help/faq'>frequently asked questions</a>
+      </li>
+      <li>
+        <a href='/help/how-to-purchase'>how to purchase</a>
+      </li>
+      <li>
+        <a href='/help/transport-and-delivery'>transport and delivery</a>
+      </li>
+      <li>
+        <a href='/help/payments'>payments</a>
+      </li>
+      <li>
+        <a href='/help/contact'>contact</a>
+      </li>
+    </List>
   </div>
   <div class='company'>
-    <ul>
-      <li>services</li>
-      <li>history of the brand</li>
-    </ul>
+    <List id='company-list' caption='company'>
+      <li>
+        <a href='/company/services'>services</a>
+      </li>
+      <li>
+        <a href='/company/history-of-the-brand'>history of the brand</a>
+      </li>
+    </List>
   </div>
   <div class='follow-us'>
-    <ul>
-      <li>facebook</li>
-      <li>twitter</li>
-      <li>youtube</li>
-      <li>instagram</li>
-    </ul>
+    <List id='follow-us-list' caption='follow us'>
+      <li>
+        <a href='https://www.facebook.com/groups/235721428293809' target='_blank'>facebook</a>
+      </li>
+      <li>
+        <a href='https://www.instagram.com/macarons_berlin/' target='_blank'>instagram</a>
+      </li>
+    </List>
   </div>
-  <div class='other'></div>
-  <div class='info'>
-    <ul>
-      <li>GDPR</li>
-      <li>imprint</li>
-    </ul>
+  <div class='legal'>
+    <List id='legal-list' isInline={true} caption='legal'>
+      <li>
+        <a href='/legal/gdpr'>GDPR</a>
+      </li>
+      <li>
+        <a href='/legal/imprint'>imprint</a>
+      </li>
+    </List>
   </div>
 </footer>
--- sources/front-end/src/routes/index.svelte
+++ sources/front-end/src/routes/index.svelte
@@ -1,5 +1,44 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
+</script>
+
+<script>
+  import {
+    onMount,
+    onDestroy,
+  } from 'svelte';
+
+  let channel;
+
+  const handleNewsletterSubscriptionRequest = ({ isTrusted, data, target }) => {
+    if (isTrusted !== true) {
+      return;
+    }
+
+    console.log('handleNewsletterSubscriptionRequest', data?.payload);
+
+    // TODO: this is for testing. remove it.
+    const to = setTimeout(() => {
+      clearTimeout(to);
+
+      target.postMessage({
+        payload: {
+          done: true,
+        },
+      });
+    }, 1000);
+
+  }
+
+  onMount(() => {
+    channel = new BroadcastChannel('newsletter-subscription');
+    channel?.addEventListener('message', handleNewsletterSubscriptionRequest)
+  });
+
+  onDestroy(() => {
+    channel?.removeEventListener('message', handleNewsletterSubscriptionRequest)
+    channel?.close();
+  });
 </script>
 
 index
