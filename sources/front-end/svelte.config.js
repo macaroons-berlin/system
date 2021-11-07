2a1c331e585bdd9a87e2f1a9c8e15312753ed5c3
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-07 17:01:41 +0100

    added service worker. works only in release builds :(

--- sources/front-end/svelte.config.js
+++ sources/front-end/svelte.config.js
@@ -1,22 +1,31 @@
 import adapterNode from '@sveltejs/adapter-node';
+// import {
+//   VitePWA,
+// } from 'vite-plugin-pwa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		format: 'esm',
-		hydratable: true,
-	},
-	extensions: [ '.svelte' ],
-	kit: {
-		adapter: adapterNode({
-			precompress: true,
-		}),
-		// hydrate the <div id="macaroons-berlin"> element in src/app.html
-		target: '#macaroons-berlin',
-		hydrate: true,
-		router: true,
-		ssr: true,
-	}
+  compilerOptions: {
+    format: 'esm',
+    hydratable: true,
+  },
+  extensions: [ '.svelte' ],
+  kit: {
+    adapter: adapterNode({
+      precompress: true,
+    }),
+    // hydrate the <div id="macaroons-berlin"> element in src/app.html
+    target: '#macaroons-berlin',
+    hydrate: true,
+    router: true,
+    ssr: true,
+    files: {
+      serviceWorker: 'src/service-worker/index.js',
+    },
+    serviceWorker: {
+      files: (filepath) => !/\.DS_STORE/.test(filepath),
+    },
+  },
 };
 
 export default config;
